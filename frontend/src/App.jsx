7676311339e9125a42c5fe3f3b47a84e66e7ea5b
theotodial <theotodial@gmail.com>
2025-12-13 01:25:56 +0500

    Add Vercel Analytics to React app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Navbar from './components/Navbar';
 import ProtectedRoute from './components/ProtectedRoute';
 import Signup from './pages/Signup';
@@ -39,6 +40,7 @@ function App() {
           } 
         />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   );
 }
